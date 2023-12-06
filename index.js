dec1dd867a0d17997dfecf27a1b04d5737a9ec0a
sahil-shaik063 <138660383+sahil-shaik063@users.noreply.github.com>
2023-12-07 00:18:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/home",(req,res)=>{
+  res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
+app.get("/services", (req, res) => {
+    res.render("services.ejs");
+  });
+
+app.get("/contact", (req, res) => {
+    res.render("contact.ejs");
+  });
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
